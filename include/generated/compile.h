ed001231133c69a9cb5e56c4a6a344aba5a6ef58
GriefNorth <grief.north@gmail.com>
2012-10-09 21:02:30 +0400

    kernel 2.6.35 with overcloking

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Tue Oct 9 21:28:00 YEKT 2012"
+#define LINUX_COMPILE_TIME "21:28:00"
+#define LINUX_COMPILE_BY "sanya"
+#define LINUX_COMPILE_HOST "sanya-Aspire-5741ZG"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
